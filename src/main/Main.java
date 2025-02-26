a59c9a5aa9accf56902c9dec41d822636dacc9ca
andreacagnina <amirkal@libero.it>
2025-02-26 12:37:20 +0100

    [feat] mod città

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,16 +8,16 @@ public class Main{
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
 
-        sistema.aggiungiUtente(new Utente("CF1", "Rende"));
-        sistema.aggiungiUtente(new Utente("CF2", "Cosenza"));
-        sistema.aggiungiUtente(new Utente("CF3", "Rende"));
+        sistema.aggiungiUtente(new Utente("CF1", "Palermo"));
+        sistema.aggiungiUtente(new Utente("CF2", "Catania"));
+        sistema.aggiungiUtente(new Utente("CF3", "Palermo"));
 
 
-        sistema.aggiungiTweet(new Tweet("Tw1", "CF1", 1, "Rende",  Arrays.asList("#capodanno", "#2020")));
-        sistema.aggiungiTweet(new Tweet("Tw2", "CF1", 1, "Cosenza", Arrays.asList("#capodanno", "#concerto")));
-        sistema.aggiungiTweet(new Tweet("Tw3", "CF2", 2, "Rende", Arrays.asList("#governo")));
-        sistema.aggiungiTweet(new Tweet("Tw4", "CF3", 1, "Rende", Arrays.asList("#capodanno")));
-        sistema.aggiungiTweet(new Tweet("Tw5", "CF3", 1, "Rende", Arrays.asList("#governo")));
+        sistema.aggiungiTweet(new Tweet("Tw1", "CF1", 1, "Palermo",  Arrays.asList("#capodanno2024", "#2023")));
+        sistema.aggiungiTweet(new Tweet("Tw2", "CF1", 1, "Catania", Arrays.asList("#capodanno2024", "#concerto")));
+        sistema.aggiungiTweet(new Tweet("Tw3", "CF2", 2, "Palermo", Arrays.asList("#governo")));
+        sistema.aggiungiTweet(new Tweet("Tw4", "CF3", 1, "Palermo", Arrays.asList("#capodanno2024")));
+        sistema.aggiungiTweet(new Tweet("Tw5", "CF3", 1, "Palermo", Arrays.asList("#governo")));
 
                 // 🔹 TEST: Utenti che hanno sempre twittato da una città diversa dalla residenza
         System.out.println("Utenti che hanno sempre twittato fuori città: " + sistema.cittaDiversa());
